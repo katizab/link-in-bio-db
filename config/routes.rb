078b30742be57af0c25b311c205922e3748e04e5
katizab <100358952+katizab@users.noreply.github.com>
2024-11-05 04:48:11 +0000

    3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+ get("/", { :controller => "items", :action => "index" })
+ 
+ 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
